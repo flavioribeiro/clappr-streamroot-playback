dfbefdaafeb6ccf124b656486628de6631f0d21e
Flávio Ribeiro <flavio.ribeiro@nytimes.com>
2016-04-03 17:01:41 -0400

    make it work with modified hlsjs

--- index.js
+++ index.js
@@ -1,10 +1,21 @@
-import {HLS, Log, Events} from 'clappr'
+import {HLS} from 'clappr'
 
 export default class StreamrootPlayback extends HLS {
   get name() {return 'streamroot_playback'}
+
+  setupHls() {
+    console.warn("Setting up HLS with", this.options.hlsjsConfig, this.options.hlsjsConfig.p2pConfig)
+    this.hls = new window.Hls(this.options.hlsjsConfig, this.options.hlsjsConfig.p2pConfig)
+    this.hls.on(Hls.Events.MEDIA_ATTACHED, () => this.hls.loadSource(this.options.src))
+    this.hls.on(Hls.Events.LEVEL_LOADED, (evt, data) => this.updatePlaybackType(evt, data))
+    this.hls.on(Hls.Events.LEVEL_UPDATED, (evt, data) => this.onLevelUpdated(evt, data))
+    this.hls.on(Hls.Events.LEVEL_SWITCH, (evt,data) => this.onLevelSwitch(evt, data))
+    this.hls.on(Hls.Events.FRAG_LOADED, (evt, data) => this.onFragmentLoaded(evt, data))
+    this.hls.on(Hls.Events.ERROR, (evt, data) => this.onError(evt, data))
+    this.hls.attachMedia(this.el)
+  }
 }
 
 StreamrootPlayback.canPlay = (resource, mimeType = '') => {
-    return true
+    return HLS.canPlay(resource, mimeType)
 }
-
