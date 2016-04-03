d03dcfa9bc8fa5de43ec8785dfdd9f3d279078db
Flávio Ribeiro <flavio.ribeiro@nytimes.com>
2016-04-03 17:23:34 -0400

    update parameters being passed to streamroot hls

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ export default class StreamrootPlayback extends HLS {
   get name() {return 'streamroot_playback'}
 
   setupHls() {
-    this.hls = new window.Hls(this.options.hlsjsConfig, this.options.hlsjsConfig.p2pConfig)
+    this.hls = new window.Hls(this.options.hlsjsConfig, this.options.p2pConfig)
     this.hls.on(Hls.Events.MEDIA_ATTACHED, () => this.hls.loadSource(this.options.src))
     this.hls.on(Hls.Events.LEVEL_LOADED, (evt, data) => this.updatePlaybackType(evt, data))
     this.hls.on(Hls.Events.LEVEL_UPDATED, (evt, data) => this.onLevelUpdated(evt, data))
