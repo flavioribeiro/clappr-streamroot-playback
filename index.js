17b0b9392beefb08400c0af3e3929510c17d2171
Kevin Canévet <kevin@streamroot.io>
2016-06-08 18:48:56 +0200

    Fix errors in the previous PR #2

--- index.js
+++ index.js
@@ -4,8 +4,10 @@ export default class StreamrootHlsjs extends HLS {
   get name() {return 'streamroot_playback'}
 
   _setupHls() {
-    this._hls = new window.Hls(this.options.hlsjsConfig || {}, this.options.p2pConfig)
-    this._hls.on(Hls.Events.MEDIA_ATTACHED, () => this._hls.loadSource(this.options.src))
+    this._hls = new window.Hls(this._options.hlsjsConfig || {}, this._options.p2pConfig)
+    this._hls.on(Hls.Events.MEDIA_ATTACHED, () => {
+        this._hls.loadSource(this._options.src)
+    });
     this._hls.on(Hls.Events.LEVEL_LOADED, (evt, data) => this._updatePlaybackType(evt, data))
     this._hls.on(Hls.Events.LEVEL_UPDATED, (evt, data) => this._onLevelUpdated(evt, data))
     this._hls.on(Hls.Events.LEVEL_SWITCH, (evt,data) => this._onLevelSwitch(evt, data))
